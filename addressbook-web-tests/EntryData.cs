c11c727b3470abcf71d2c1a91b902737ab3cb62f
Aleksandr Kozelskikh <a.n.kozelskikh@gmail.com>
2021-05-20 22:43:34 +0500

    Задание №4: Сделать всё то же самое, но для контактов. (Добавление контакта)

---
+++ addressbook-web-tests/EntryData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class EntryData
+    {
+        public string firstname;
+        public string lastname = "";
+
+        public EntryData (string firstname)
+        {
+            this.firstname = firstname;
+            
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+
+    }
+}
